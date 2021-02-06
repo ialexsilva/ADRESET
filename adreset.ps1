f4a4f563a4a7d2556ef547ed8f3090aad6f267d6
Alex Silva <ialexsilva@users.noreply.github.com>
2021-02-06 11:03:01 -0300

    Update adreset.ps1

--- adreset.ps1
+++ adreset.ps1
@@ -3,23 +3,31 @@ if (-not (Get-Module ActiveDirectory)){
 
 }
 
-function ChangePassword {
+function changePassword {
 $username = read-host 'Nome de usuario:'
 $pass = ConvertTo-SecureString 'Sysmap*2021' -AsPlainText -Force
 $adminCred = Get-Credential -Message  'Você precisar inserir as credenciais para desbloqueio da função de reset de senha.'
 Set-ADAccountPassword $username -NewPassword $pass -PassThru -Credential $adminCred -Server SYSMAP.com.br
-Enable-ADAccount $username -Server SYSMAP.com.br
+menu
+}
+#
+function unlockAccount {
+$username = read-host 'Nome de usuario:'
+$adminCred = Get-Credential -Message  'Você precisar inserir as credenciais para desbloqueio da função de reset de senha.'
+Enable-ADAccount $username -Server SYSMAP.com.br -Credential $adminCred
 menu
 }
 #
 function menu {
 $stepChoice = read-host "
 1. Reset de Senha
+2. Desbloqueio de usuário
 0. Sair
 "
 switch($stepChoice)
 {
-    1{ChangePassword}
+    1{changePassword}
+    2{unclockAccount}
     0{exit}
 
 }
