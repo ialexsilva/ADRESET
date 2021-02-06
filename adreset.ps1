55e691e73bbe68104de7e27b0ff927a1d90f76aa
Alex Silva <ialexsilva@users.noreply.github.com>
2021-02-06 10:49:35 -0300

    Create adreset.ps1

---
+++ adreset.ps1
@@ -0,0 +1,27 @@
+if (-not (Get-Module ActiveDirectory)){    
+ Import-Module ActiveDirectory -ErrorAction Stop
+
+}
+
+function ChangePassword {
+$username = read-host 'Nome de usuario:'
+$pass = ConvertTo-SecureString 'Sysmap*2021' -AsPlainText -Force
+$adminCred = Get-Credential -Message  'Você precisar inserir as credenciais para desbloqueio da função.'
+Set-ADAccountPassword $username -NewPassword $pass -PassThru -Credential $adminCred
+Enable-ADAccount $SamAccountName
+menu
+}
+#
+function menu {
+$stepChoice = read-host "
+1. Reset de Senha
+0. Sair
+"
+switch($stepChoice)
+{
+    1{ChangePassword}
+    0{exit}
+
+}
+}
+menu
